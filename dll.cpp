e58c9921e9067db25769ffe468fe79dab31d3f81
thedumbell <test000009990@gmail.com>
2025-02-07 02:48:30 +0300

    ş

--- dll.cpp
+++ dll.cpp
@@ -1,13 +1,13 @@
 #include <windows.h>
 
-DWORD WINAPI MessageThread(LPVOID lpParam) {
+void ShowMessageBox() {
     MessageBoxA(NULL, "Hello from injected DLL!", "Injected", MB_OK | MB_ICONINFORMATION);
-    return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        CreateThread(NULL, 0, MessageThread, NULL, 0, NULL);
+        DisableThreadLibraryCalls(hModule); // Gereksiz thread çağrılarını kapat
+        ShowMessageBox();  // Doğrudan çağır (CreateThread kullanmadan)
     }
     return TRUE;
 }
